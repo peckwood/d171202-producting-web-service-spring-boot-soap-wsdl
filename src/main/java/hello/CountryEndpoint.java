93b3746e8c60cd3dd76ed800d74ce20aa1729445
raiden <peckwood@users.noreply.github.com>
2017-12-08 17:37:40 +0800

    initial not working

---
+++ src/main/java/hello/CountryEndpoint.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+//@Endpoint registers the class with Spring WS as a potential candidate for processing incoming SOAP messages.
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+	@Autowired
+	private CountryRepository countryRepository;
+	
+	//@PayloadRoot is then used by Spring WS to pick the handler method based on the message’s namespace and localPart.
+	@PayloadRoot(namespace=NAMESPACE_URI, localPart="getCountryRequest")
+	//The @ResponsePayload annotation makes Spring WS map the returned value to the response payload.
+	@ResponsePayload
+	//@RequestPayload indicates that the incoming message will be mapped to the method’s request parameter.
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request){
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+		return response;
+	}
+}
---
+++ src/main/java/hello/WebServiceConfig.java
@@ -0,0 +1,50 @@
+package hello;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter{
+	
+	/*
+	 * It’s important to notice that you need to specify bean names for  
+	 * MessageDispatcherServlet and DefaultWsdl11Definition. Bean names 
+	 * determine the URL under which web service and the generated WSDL 
+	 * file is available. In this case, the WSDL will be available under
+	 *  http://<host>:<port>/ws/countries.wsdl.
+	 *  In this case: http://localhost:8080/ws/countries.wsdl
+	 */
+	
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext){
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(servlet, "/ws/*");
+	}
+	
+	@Bean
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema){
+		DefaultWsdl11Definition wsdl11Definition= new DefaultWsdl11Definition();
+		wsdl11Definition.setPortTypeName("CountriesPort");
+		wsdl11Definition.setLocationUri("/ws");
+		wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
+		wsdl11Definition.setSchema(countriesSchema);
+		return wsdl11Definition;
+	}
+	
+	@Bean
+	public XsdSchema countriesSchema(){
+		return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
+	}
+}
